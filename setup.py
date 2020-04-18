182cb798977f8795b5f5b1ec5e1f637b56530022
waltermas <waltermas@hotmail.com>
2020-04-18 17:23:09 -0300

    version3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'chuletas',
   packages = ['chuletas'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'command info',
   author = 'waltermas-gitter',
   author_email = 'waltermas@hotmail.com',
   url = 'https://github.com/waltermas-gitter/chuletas}', # use the URL to the github repo
-  download_url = 'https://github.com/waltermas-gitter/chuletas/tarball/0.2',
+  download_url = 'https://github.com/waltermas-gitter/chuletas/tarball/0.3',
   keywords = ['testing', 'logging', 'example'],
   classifiers = [],
   scripts=['bin/chuletas']
